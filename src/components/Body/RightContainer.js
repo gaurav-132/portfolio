3da0b7c622952139cc48ea3d2c3b63e5ef143da9
gaurav-132 <ggusain264@gmail.com>
2024-04-01 21:58:40 +0530

    Make reponsive right container

--- src/components/Body/RightContainer.js
+++ src/components/Body/RightContainer.js
@@ -30,19 +30,19 @@ const RightContainer = () => {
     return (
       <div>
         <div className={`w-full  bg-cover bg-no-repeat bg-[url('https://ez-snippet.vercel.app/images/cover.jpeg')]`}>
-            <div className='flex justify-center gap-2 md:gap-10 md:px-10 py-5 bg-blur backdrop-filter backdrop-blur-md'>
+            <div className='flex resp-main justify-center gap-2 md:gap-10 md:px-10 py-5 bg-blur backdrop-filter backdrop-blur-md'>
                 <div className=''>
-                    <img className='rounded-full border-4 object-cover' width={120} src='https://avatars.githubusercontent.com/u/109432749?v=4'/>
+                    <img className='rounded-full border-4 object-cover img-resp' width={120} src='https://avatars.githubusercontent.com/u/109432749?v=4'/>
                 </div>
                 <div className='flex md:flex-1 justify-between items-center gap-4'>
-                    <div className='text-3xl text-white'>
+                    <div className='text-3xl text-white title-resp'>
                         Programming Hub
                     </div>
-                    <div>
-                    <a className='bg-primary cursor-pointer text-textPrimary px-2 py-2 rounded-md'>
-                        <FontAwesomeIcon icon={["fab", "instagram"]} size='lg'/>
-                        <span className='px-2'>Follow me on Instagram</span>
-                    </a>
+                    <div className=' resp-insta'>
+                        <a className='bg-primary cursor-pointer text-textPrimary px-2 py-2 rounded-md'>
+                            <FontAwesomeIcon icon={["fab", "instagram"]} size='lg'/>
+                            <span className='px-2'>Follow me on Instagram</span>
+                        </a>
                     </div>
                 </div>
             </div>
